ac7464d98168ab134658261a406961bd30d8aa38
Victor Ribeiro <hyukodev@gmail.com>
2022-05-01 18:17:46 -0300

    feat: add translation for 'signin' page

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,3 +1,5 @@
+import { useTranslation } from 'next-i18next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import { Button, Center, Container, Heading, VStack } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 import React from 'react';
@@ -5,6 +7,7 @@ import { FcGoogle } from 'react-icons/fc';
 import { useAuth } from '../lib/auth';
 
 const signin = () => {
+  const { t } = useTranslation('signin')
   const { auth, siginWithGoogle } = useAuth();
   const router = useRouter();
 
@@ -13,19 +16,29 @@ const signin = () => {
   }
 
   return (
-    <Container>
-      <Center mt={10}>
-        <VStack spacing="4">
-          <Heading fontSize="3xl" mb={2}>
-            Hello, Welcome to the Quiz App!!
-          </Heading>
-          <Button leftIcon={<FcGoogle />} onClick={() => siginWithGoogle()}>
-            Sign In with Google
-          </Button>
-        </VStack>
-      </Center>
+    <Container
+      maxW="3xl"
+      mt={5}
+      p={6}
+    >
+      <VStack spacing="4">
+        <Heading fontSize="3xl" mb={2}>
+          {t('welcome')}
+        </Heading>
+        <Button size='lg' leftIcon={<FcGoogle />} onClick={() => siginWithGoogle()}>
+          {t('signInWithGoogle')}
+        </Button>
+      </VStack>
     </Container>
   );
 };
 
 export default signin;
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common', 'signin', 'footer'])),
+    },
+  };
+}
