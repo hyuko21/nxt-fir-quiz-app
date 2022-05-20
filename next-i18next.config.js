7e61f5e3c840ec2f9d7ed7c918b540ff6b68e20d
Victor Ribeiro <hyukodev@gmail.com>
2022-05-20 17:19:12 -0300

    chore: move locales to public folder

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,9 @@
+const path = require('path');
+
 module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'pt'],
-    localePath: './locales',
+    localePath: path.resolve('public', 'locales'),
   },
 }
